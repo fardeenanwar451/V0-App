b6d4cbb46131781cec4ee2c32e49801dcc1b9089
Fardeen451 <fardeenanwar45@gmail.com>
2026-01-17 04:07:25 +0530

    Dockerized DB added

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Button>Test</Button>
+    </div>
+  );
+}
